2855dd464b013fc94f4bb9947a623cce6b347bcb
RahmanM <rahman.mahmoodi@gmail.com>
2016-10-26 21:30:15 +1100

    init

---
+++ App/Controllers/EPController.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../AppConfig.js" />
+
+app.controller("EPController", ['$scope', 'EPService', function ($scope, EPService) {
+
+    var EPCtrl = this;
+    EPCtrl.Employee = {};
+    EPCtrl.Departments = [];
+
+    EPCtrl.Init = function() {
+        EPCtrl.Employee = EPService.getEmployee();
+        EPCtrl.Departments = ["Development", "Services", "Support"];
+    }
+
+    EPCtrl.Init();
+
+    EPCtrl.submitForm = function() {
+        alert("got it");
+        return;
+    }
+}]);
---
+++ App/Services/EPService.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../AppConfig.js" />
+
+app.service("EPService", function () {
+
+    var self = this;
+
+    self.employee = {
+        firstName: "Rahman",
+        lastName: "Mahmoodi",
+        department: "Dev",
+        perks: ["car", "stock"],
+        payrollType : "1"
+    };
+
+    self.getEmployee = function () {
+        return self.employee;
+    }
+    
+});
